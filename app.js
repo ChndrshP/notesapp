366a06a24c4c1a9ebe0c68c6ce04ef5cb183162c
Chandresh <chndrsh25@gmail.com>
2024-03-20 10:04:18 +0530

    app finished

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 import express  from 'express';
+import { getNote,
+         getNotes,
+         createNote} 
+         from './database.js';   
+
 
 const app = express();
 
+app.use(express.json());
+    
+//Fetching notes
+app.get("/notes", async (req, res) => {
+    const notes = await getNotes();
+    res.send(notes);
+})
 
+app.get("/note/:id", async (req, res) => {
+    const id = req.params.id;
+    const note = await getNote(id);
+    res.send(note);
+})
 
-
-
+app.post("/create-note", async (req, res) =>{
+    const {title, contents} = req.body;
+    const note = await createNote(title, contents);
+    res.status(201).send(note);
+})
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 })
-
 
 app.listen(8080, () =>{
     console.log('server running on 8080');
